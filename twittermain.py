151db25b136fc34cf89f1dc7bfae14e143ac9967
Ramadida Rai Pahlevi <Ramadida@Ramadidas-MacBook-Pro.local>
2019-11-29 13:18:32 +0700

    Modified front end template and Twitter API blm jadi wkwk

---
+++ twittermain.py
@@ -0,0 +1,14 @@
+import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+# Fill the X's with the credentials obtained by
+# following the above mentioned procedure.
+CONSUMER_KEY = "vun19WFwsJNMOG8zTcWIgHaRP"
+CONSUMER_SECRET = "6vP95kbUUdHGqCYtY4DXfNyJTa2wFdTf598jKZazKqiKOjSJNu"
+ACCESS_KEY = "83359542-CbOtYl25zVKKlVaAEhPvsa3oHq9f73JGNanTC5nit"
+ACCESS_SECRET = "sNZhq5WpXTAqo0aecYruGYcXnx1W5RISqXhGGycklMFxE"
+
+
+# Function to extract tweets
