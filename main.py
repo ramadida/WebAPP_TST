889d8083ebaf333e8ad5b88ec18f939fd26d32af
Ramadida Rai Pahlevi <Ramadida@Ramadidas-MacBook-Pro.local>
2019-11-29 20:00:38 +0700

    Updated route for twitter

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
 import requests
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def root():
-	data = requests.get('http://localhost:5001/movie/now_playing').json()
-	return render_template('nowplaying.html', nps = data)
+    data = requests.get('http://localhost:5001/movie/now_playing').json()
+    return render_template('nowplaying.html', nps=data)
+
+
+@app.route('/getTweetsByFilter/<search_word>')
+def movietweets(search_word):
+    try:
+        tweets = requests.get('http://localhost:5000/getTweetsByFilter/<search_word>')
+        return render_template('single_nowplaying.html', Tweet=tweets.text, Username="@" + tweets.user.screen_name)
+    except Exception as e:
+        raise e
+
 
 if __name__ == '__main__':
-	#app.run(threaded = True, port = 5000)
-    app.run()
+    app.run(port=5001, debug=True)
