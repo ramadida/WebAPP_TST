3f5503c9c00c5bfbf9c0c00c9ebfa1a1d06e9ac9
Ramadida Rai Pahlevi <Ramadida@Ramadidas-MacBook-Pro.local>
2019-11-29 17:30:16 +0700

    Function filtersearch_tweets updated

--- twittermain.py
+++ twittermain.py
@@ -1,14 +1,24 @@
 import tweepy
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
 
+from flask import Flask
+app = Flask(__name__)
 # Fill the X's with the credentials obtained by
 # following the above mentioned procedure.
-CONSUMER_KEY = "vun19WFwsJNMOG8zTcWIgHaRP"
-CONSUMER_SECRET = "6vP95kbUUdHGqCYtY4DXfNyJTa2wFdTf598jKZazKqiKOjSJNu"
-ACCESS_KEY = "83359542-CbOtYl25zVKKlVaAEhPvsa3oHq9f73JGNanTC5nit"
-ACCESS_SECRET = "sNZhq5WpXTAqo0aecYruGYcXnx1W5RISqXhGGycklMFxE"
+consumer_key = "vun19WFwsJNMOG8zTcWIgHaRP"
+consumer_secret = "6vP95kbUUdHGqCYtY4DXfNyJTa2wFdTf598jKZazKqiKOjSJNu"
+access_token = "83359542-CbOtYl25zVKKlVaAEhPvsa3oHq9f73JGNanTC5nit"
+access_secret = "sNZhq5WpXTAqo0aecYruGYcXnx1W5RISqXhGGycklMFxE"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+# API object
+api = tweepy.API(auth)
 
 
-# Function to extract tweets
+def filtersearch_tweets(search_word):
+    for tweet in api.search(q=search_word, result_type="popular"):
+        print(f"{tweet.user.name}:{tweet.text}")
+
+
+filtersearch_tweets("Unai Emery")
